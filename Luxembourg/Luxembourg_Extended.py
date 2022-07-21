9917e3539886f0cf1b8a778e28da84fc3d550dcd
mpssalazar <mpssalazar@gmail.com>
2022-07-21 22:18:17 +0200

    test

--- Luxembourg/Luxembourg_Extended.py
+++ Luxembourg/Luxembourg_Extended.py
@@ -91,7 +91,7 @@ def treat_country(dataframe):
 
 
 # Load data on prices
-apart_sale_luxcity = pd.read_excel('./Resources/announced-prices-apartments-luxembourg-city.xlsx')
+apart_sale_luxcity = pd.read_excel('/Resources/announced-prices-apartments-luxembourg-city.xlsx')
 apart_rent_luxcity = pd.read_excel('./Resources/announced-rent-apartments-luxembourg-city.xlsx')
 house_sale_luxcity = pd.read_excel('./Resources/announced-prices-houses-luxembourg-city.xlsx')
 apart_sale_luxcountry = pd.read_excel('./Resources/registered-prices-apartements-by-commune.xlsx')
